5fd88a8c5578e7b7a410ea8bcafa6f3bf29368a9
shwephuehmone25 <shwephue7889@gmail.com>
2024-03-26 10:14:29 +0630

    tags & programmings api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\TagController;
+use App\Http\Controllers\Admin\ProgrammingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/**TAG*/
+Route::get('/tags/getAll', [TagController::class, 'index']);
+Route::post('/tag/create', [TagController::class, 'store']);
+Route::post('/tag/update', [TagController::class, 'update']);
+Route::get('/tag/delete', [TagController::class, 'destroy']);
+
+/**PROGRAMMING*/
+Route::get('/programs/getAll', [ProgrammingController::class, 'index']);
+Route::post('/program/create', [ProgrammingController::class, 'store']);
+Route::post('/program/update', [ProgrammingController::class, 'update']);
+Route::get('/program/delete', [ProgrammingController::class, 'destroy']);
